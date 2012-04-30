ab2776a6ff6a6ac87438cb2537bc4a23908f07df
Iván Guillén <zeopix@gmail.com>
2012-04-30 08:04:02 +0200

    fondo

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,39 @@
-$(function(){
+function LoginGetHeaders(user,token,pass){
+	return '/UsernameToken Username="'+user+'", Token="'+token+'", PasswordDigest="'+pass+'"';
+}
 
-	$("a.loginsubmit.enabled").click(function(){
-
-		var user = $(".form.login #email").val();
-		var pass = $(".form.login #password").val();
-		
-		$.ajax({
+function LoginAction(user,pass,token){
+	$.ajax({
 			type:'GET',
 			url: loginurl,
 			headers: {
-				'x-token' : '/UsernameToken Username="'+user+'", PasswordDigest="'+pass+'"'
+				'x-token' : LoginGetHeaders(user,token,pass)
 			},
 			success: function(response,status){
 				if(response.status == 200){
 					alert("Te has logueado con éxito!");
+					//proceed to username&store token
+					
+					
 				}else{
+				
 					alert("Email/Contraseña incorrectos");
 				}
 			},error: function(){
 			alert("Email/Contraseña incorrectos");
 			}
-		});		
+	});	
+}
+
+$(function(){
+
+	$("a.loginsubmit").click(function(){
+
+		var user = $(".form.login #email").val();
+		var pass = $(".form.login #password").val();
+		var token = "null";
+		
+		//LoginAction(user,pass,token);	
 	});
 
 })
